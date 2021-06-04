07c9dce167fdd0f6a47532c39bcbc1277b160671
jaujavi <javier.torre.antunez@gmail.com>
2021-06-04 21:44:04 +0200

    testing multiple image upload

--- s3-upload-images.py
+++ s3-upload-images.py
@@ -1,15 +1,22 @@
 import logging
 import boto3
 from botocore.exceptions import ClientError
+import os
 
-file_name = 'images/0_puppies.jpg'
-bucket = 'jaujavi-oldbucket'
-object_name = file_name
+dir_images = 'images/'
+bucket_name = 'jaujavi-oldbucket'
 
-# Upload the file
-s3_client = boto3.client('s3')
-try:
-	response = s3_client.upload_file(file_name, bucket, object_name, ExtraArgs={'ACL':'public-read'})
-	print(response)
-except ClientError as e:
-	logging.error(e)
+for root, dirs, files in os.walk(dir_images):
+	for file in files:
+		if file.endswith(".jpg"):
+			file_and_path = os.path.join(root, file)
+			print(file_and_path)
+			object_name = file_and_path
+
+			# Upload the file
+			s3_client = boto3.client('s3')
+			try:
+				response = s3_client.upload_file(file_and_path, bucket_name, object_name, ExtraArgs={'ACL':'public-read'})
+				print(response)
+			except ClientError as e:
+				logging.error(e)

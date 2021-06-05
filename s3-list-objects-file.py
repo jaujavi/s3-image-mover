ddc1bcee8efc935de292123165f3b4e31169036d
jaujavi <javier.torre.antunez@gmail.com>
2021-06-05 13:17:29 +0200

    testing objects listing dictionary

--- s3-list-objects-file.py
+++ s3-list-objects-file.py
@@ -5,19 +5,22 @@ def list_objects_file (bucket_name, file_name):
 	s3 = boto3.resource('s3')
 	bucket = s3.Bucket(bucket_name)
 
-	print('=> LISTING OBJECTS FROM:',bucket)
+	print('=> LISTING OBJECTS FROM:',bucket.name)
 
 	objects_file = open(file_name,'w')
 	for s3_object in bucket.objects.all():
     	#print(s3_object.key)
 		objects_file.write(s3_object.key + '\n')
 
-#main
-s3_oldbucket = 'jaujavi-oldbucket'
-s3_newbucket = 'jaujavi-newbucket'
+oldbucket = {
+	'name':'jaujavi-oldbucket',
+	'file':'s3_oldbucket_objects.txt'
+}
 
-s3_oldbucket_file = 's3_oldbucket_objects.txt'
-s3_newbucket_file = 's3_newbucket_objects.txt'
+newbucket = {
+	'name':'jaujavi-newbucket',
+	'file':'s3_newbucket_objects.txt'
+}
 
-list_objects_file(s3_oldbucket,s3_oldbucket_file)
-list_objects_file(s3_newbucket,s3_newbucket_file)
+list_objects_file(oldbucket['name'],oldbucket['file'])
+list_objects_file(newbucket['name'],newbucket['file'])

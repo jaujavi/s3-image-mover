403cda188dd5c08b8c684ea9ce08f873b4c0b967
jaujavi <javier.torre.antunez@gmail.com>
2021-06-05 12:46:27 +0200

    testing objects listing function

--- s3-list-objects-file.py
+++ s3-list-objects-file.py
@@ -1,15 +1,23 @@
 import boto3
 import os
 
+def list_objects_file (bucket_name, file_name):
+	s3 = boto3.resource('s3')
+	bucket = s3.Bucket(bucket_name)
+
+	print('=> LISTING OBJECTS FROM:',bucket)
+
+	objects_file = open(file_name,'w')
+	for s3_object in bucket.objects.all():
+    	#print(s3_object.key)
+		objects_file.write(s3_object.key + '\n')
+
+#main
 s3_oldbucket = 'jaujavi-oldbucket'
 s3_newbucket = 'jaujavi-newbucket'
 
-s3_oldbucket_file = open('s3_oldbucket_objects.txt','w')
-s3_newbucket_file = open('s3_newbucket_objects.txt','w')
+s3_oldbucket_file = 's3_oldbucket_objects.txt'
+s3_newbucket_file = 's3_newbucket_objects.txt'
 
-s3 = boto3.resource('s3')
-oldbucket = s3.Bucket(s3_oldbucket)
-print('=> LISTING OBJECTS FROM:',s3_oldbucket)
-for s3_file in oldbucket.objects.all():
-    #print(s3_file.key)
-	s3_oldbucket_file.write(s3_file.key + '\n')
+list_objects_file(s3_oldbucket,s3_oldbucket_file)
+list_objects_file(s3_newbucket,s3_newbucket_file)

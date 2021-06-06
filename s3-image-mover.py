ee52a8daf97d6c0c52fb87842ba8fb0c9c6daf19
jaujavi <javier.torre.antunez@gmail.com>
2021-06-06 22:04:19 +0200

    cleaning operations implemented

--- s3-image-mover.py
+++ s3-image-mover.py
@@ -7,7 +7,7 @@ def ls_objects_file (bucket_name, file_name):
 	s3 = boto3.resource('s3')
 	bucket = s3.Bucket(bucket_name)
 
-	print('=> LISTING OBJECTS FROM:',bucket.name)
+	print('---LISTING OBJECTS FROM:',bucket.name,'->',file_name + '\n')
 
 	objects_file = open(file_name,'w')
 	for s3_object in bucket.objects.all():
@@ -30,7 +30,7 @@ def object_moved (object_name, file_name):
 
 #copy all objects from a list from one bucket to another
 def cp_objects (bucket_old, objects_list, bucket_new):
-
+	print('---START COPYING OBJECTS:',bucket_old,'=>',bucket_new + '\n')
 	with open(objects_list, 'r') as file_objects:
 		for s3_object in file_objects:
 			s3_object = s3_object.replace('\n', '')
@@ -41,24 +41,85 @@ def cp_objects (bucket_old, objects_list, bucket_new):
 			}
 
 			if not object_moved(s3_object, newbucket['Moved_Log']):
-				print('COPYING:', copy_source['Bucket'], copy_source['Key'], '=>', bucket_new)
+				print('INFO: Copying object:', copy_source['Bucket'], copy_source['Key'], '=>', bucket_new)
 				start_time_copy = time.time()
 
 				try:
-					#s3 = boto3.resource('s3')
-					#bucket = s3.Bucket(bucket_new)
-					#bucket.copy(copy_source, s3_object)
-					print('--- bucket opetation ---')
+					s3 = boto3.resource('s3')
+					bucket = s3.Bucket(bucket_new)
+					bucket.copy(copy_source, s3_object)
 				except:
-					print('COPY ERROR =>', copy_source['Key'])
+					print('ERROR: Object copy error =>', copy_source['Key'])
 				else:
 					with open(newbucket['Moved_Log'],'a') as file_moved:
 						file_moved.write(copy_source['Key'] + '\n')
-					print('OBJECT MOVED =>', copy_source['Key'])
+					print('INFO: Object moved =>', copy_source['Key'])
 
 				print('Execution time: ' + str((time.time() - start_time_copy)) + '\n')
 			else:
 				print('WARN: OBJECT ALREADY MOVED ->', s3_object)
+
+#count objects from a bucket
+def count_objects(bucket):
+	totalCount = 0
+	for key in bucket.objects.all():
+		totalCount += 1
+	return totalCount
+
+#compare the number of objects from two buckets
+def compare_buckets(bucket1, bucket2):
+	s3 = boto3.resource('s3')
+	print('INFO:',bucket1,'->',count_objects(s3.Bucket(bucket1)),'objects found.')
+	print('INFO:',bucket2,'->',count_objects(s3.Bucket(bucket2)),'objects found.')
+	return count_objects(s3.Bucket(bucket1)) == count_objects(s3.Bucket(bucket2))
+
+#remove a file
+def remove_file(file):
+		if os.path.isfile(file):
+			os.remove(file)
+			print('INFO: File',file,'removed.')
+		else:
+			print('ERROR: The file',file,'does not exist.')
+
+#empty a bucket
+def empty_bucket(bucket_name):
+	try:
+		s3 = boto3.resource('s3')
+		bucket = s3.Bucket(bucket_name)
+		bucket.objects.all().delete()
+	except Exception as e:
+		print('ERROR: Clean bucket error ->',e)
+	else:
+		print('INFO: Bucket',bucket_name,'empty.')
+
+#delete a bucket
+def delete_bucket(bucket_name):
+	try:
+		s3 = boto3.resource('s3')
+		bucket = s3.Bucket(bucket_name)
+		bucket.delete()
+	except Exception as e:
+		print('ERROR: Delete bucket error ->',e)
+	else:
+		print('INFO: Bucket',bucket_name,'deleted.')
+
+#cleaning operations
+def clean_ops(oldbucket, newbucket):
+	print('---CLEANING OPERATIONS')
+	if compare_buckets(oldbucket['Name'], newbucket['Name']):
+		try:
+			remove_file(oldbucket['File'])
+			remove_file(newbucket['File'])
+			remove_file(newbucket['Moved_Log'])
+			empty_bucket(oldbucket['Name']),
+			#delete_bucket(oldbucket['Name']),
+			compare_buckets(oldbucket['Name'], newbucket['Name'])
+		except:
+			print('ERROR: Clean operations failed.')
+		finally:
+			print('INFO: PROGRAM FINISHED.'+ '\n')
+	else:
+		print('ERROR: Different number of objects detected on both buckets. Please check.')
 
 
 #---main
@@ -75,10 +136,10 @@ newbucket = {
 }
 
 start_time = time.time()
-print('---START COPYING' + '\n')
 
 ls_objects_file(oldbucket['Name'],oldbucket['File'])
 cp_objects(oldbucket['Name'], oldbucket['File'], newbucket['Name'])
 ls_objects_file(newbucket['Name'],newbucket['File'])
+clean_ops(oldbucket,newbucket)
 
-print('---TOTAL EXECUTION TIME: ' + str((time.time() - start_time)) + '\n')
+print('\n' + '---TOTAL EXECUTION TIME: ' + str((time.time() - start_time)) + '\n')
